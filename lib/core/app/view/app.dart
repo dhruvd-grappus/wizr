28e8aab6c1a862553f35623107bed2e6671f57f4
deepanshu <deepanshuharbola@gmail.com>
2023-06-16 11:06:30 +0530

    added screen utils for responsive page and font size.

--- lib/core/app/view/app.dart
+++ lib/core/app/view/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:wizr/core/l10n/l10n.dart';
 import 'package:wizr/core/navigation/go_router_config.dart';
 import 'package:wizr/core/theme/theme_constants.dart';
@@ -8,11 +9,14 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: darkTheme,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      routerConfig: router,
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      builder: (_, __) => MaterialApp.router(
+        theme: darkTheme,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        routerConfig: router,
+      ),
     );
   }
 }
